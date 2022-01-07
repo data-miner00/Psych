0bf1a173d01fdb0335db28bf70475505c033a112
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-07 23:28:07 +0800

    feat: Added read from file sample

---
+++ Concepts/IO/ReadFromFile.fs
@@ -0,0 +1,14 @@
+namespace Psych.Concepts.IO
+
+open System
+open System.IO
+
+let lines = File.ReadAllLines(@"C:\programs\file.txt")
+
+// Convert file lines into a list.
+let list = Seq.toList lines
+printfn "%A" list
+
+// Get sequence of only capitalized lines in list.
+let uppercase = Seq.where (fun (n : String) -> Char.IsUpper(n, 0)) list
+printfn "%A" uppercase
