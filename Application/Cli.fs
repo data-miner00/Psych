98f58b17f9bb370367dfdedd772ca9f056ecb843
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-18 23:26:36 +0800

    chore: Added application project

---
+++ Application/Cli.fs
@@ -0,0 +1,7 @@
+﻿namespace Psych.Application
+
+module Cli =
+    
+    let welcome () =
+        printfn "Welcome to XXX Bank"
+
---
+++ Application/Program.fs
@@ -0,0 +1,10 @@
+﻿namespace Psych.Application
+// Bank application
+open System
+
+module Program =
+
+    [<EntryPoint>]
+    let main argv =
+        Cli.welcome()
+        0 // return an integer exit code
