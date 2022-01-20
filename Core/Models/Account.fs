e3217bf9b9b99162b3fbc086b86e5649b272af8c
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-20 22:05:11 +0800

    feat: Added account model

---
+++ Core/Models/Account.fs
@@ -0,0 +1,17 @@
+﻿namespace Psych.Core.Models
+
+open System
+
+type AccountType =
+| Savings = 0
+| Current = 1
+| Investment = 2
+
+type Account(createdAt: DateTime, updatedAt: DateTime, id: string, _type: AccountType,
+    balance: float, ownerId: string) =
+    member this.CreatedAt = createdAt
+    member this.UpdatedAt = updatedAt
+    member this.Id = id
+    member this.Type = _type
+    member this.Balance = balance
+    member this.OwnerId = ownerId
