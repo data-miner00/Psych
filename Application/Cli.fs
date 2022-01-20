e69b7f4997796a255461394acd4992860ae387a7
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-20 22:05:18 +0800

    feat: Updated cli

--- Application/Cli.fs
+++ Application/Cli.fs
@@ -1,7 +1,25 @@
 ﻿namespace Psych.Application
 
+open System
+
 module Cli =
+
+    let login () =
+        0
+
+    let register () =
+        0
     
     let welcome () =
         printfn "Welcome to XXX Bank"
+        printfn "1. Login"
+        printfn "2. Register"
+        printf "> "
+        let selection = Console.ReadLine()
 
+        let a = match selection with
+                | "1" -> login()
+                | "2" -> register()
+                | _ -> 0
+
+        printfn "%d" a
