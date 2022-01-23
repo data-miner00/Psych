6ed834b18027ee7d389293d026c84e8509b79670
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-23 15:21:11 +0800

    feat: Added playground project

---
+++ Playground/Program.fs
@@ -0,0 +1,9 @@
+﻿namespace Psych.Playground
+
+open System
+
+module Program =
+    [<EntryPoint>]
+    let main argv =
+        printfn "Hello World from F#!"
+        0 // return an integer exit code
