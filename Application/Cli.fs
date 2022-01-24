db00f7548eab46d83425b7e33f5381cf1f8c0dc3
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-24 20:45:31 +0800

    feat: Updated cli

--- Application/Cli.fs
+++ Application/Cli.fs
@@ -1,15 +1,43 @@
 ﻿namespace Psych.Application
 
 open System
+open Psych.Data
+open Psych.Core.Models
 
 module Cli =
 
     let login () =
-        0
+        printf "Enter your username: "
+        let username = Console.ReadLine()
+        printf "Enter your password: "
+        let password = Console.ReadLine()
+
+        MemoryUserData.getUser username
 
     let register () =
-        0
-    
+        printf "Enter your first name: "
+        let firstName = Console.ReadLine()
+        printf "Enter your last name: "
+        let lastName = Console.ReadLine()
+        printf "Enter your user name: "
+        let username = Console.ReadLine()
+        printf "Enter your age: "
+        let age = Console.ReadLine() |> Convert.ToInt32
+        printf "Enter your birthday in dd/MM/yyyy format: "
+        let birthday = Console.ReadLine()
+        printf "Enter your address: "
+        let location = Console.ReadLine()
+        printf "Enter your email address: "
+        let email = Console.ReadLine()
+        printf "Enter your password: "
+        let password = Console.ReadLine()
+        printf "Confirm your password: "
+        let confirm = Console.ReadLine()
+
+        User(firstName, lastName, username, age, 0.0, location, email, password, DateTime.Now)
+        |> MemoryUserData.addUser
+        |> Some
+
     let welcome () =
         printfn "Welcome to XXX Bank"
         printfn "1. Login"
@@ -17,9 +45,15 @@ module Cli =
         printf "> "
         let selection = Console.ReadLine()
 
-        let a = match selection with
-                | "1" -> login()
-                | "2" -> register()
-                | _ -> 0
+        let useropt = match selection with
+                        | "1" -> login()
+                        | "2" -> register()
+                        | _ -> login()
 
-        printfn "%d" a
+        match useropt with
+        | Some(user) ->
+            printfn "%s" user.FirstName
+            printfn "%s" user.Email
+            printfn "%d" user.Age
+        | None -> printf "sadge thing happend"
+    
