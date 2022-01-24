16c8b7b12bcd5e353b0f2a3735b478107cc9dc1c
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-24 20:44:59 +0800

    feat: Added Data project

---
+++ Data/MemoryUserData.fs
@@ -0,0 +1,35 @@
+﻿namespace Psych.Data
+
+open System
+open System.Collections.Generic
+open Psych.Core.Models
+
+// Side Effect
+module MemoryUserData =
+    let mutable private users = List<User>()
+
+    let addUser (user: User) = 
+        users.Add(user) |> ignore
+        user
+
+    let getUser (username: string) =
+        try users.Find(fun x -> x.Username = username) |> Some
+        with :? ArgumentNullException -> None
+
+    let updateUser (user: User) =
+        match getUser user.Username with
+        | Some(user') ->
+            users.Remove(user') |> ignore
+            users.Add(user)
+        | None -> raise (InvalidOperationException "Cannot find user with given username")
+
+    let deleteUser (username: string) =
+        match getUser username with
+        | Some(user') ->
+            users.Remove(user') |> ignore
+        | None -> raise (InvalidOperationException "Cannot find user with given username")
+
+// Non side effect
+module MemoryUserData' =
+    let private users: User[] = [||]
+
