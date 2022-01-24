704867e102fd44e8317e7715806493c645714a38
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-24 20:39:22 +0800

    feat: Added debug format concept

---
+++ Concepts/Format/Debug.fs
@@ -0,0 +1,27 @@
+﻿namespace Psych.Concepts.Format
+
+open System
+open FSharp.Reflection
+
+// Reference: https://stackoverflow.com/questions/11559440/how-to-manage-debug-printing-in-f
+module Debug =
+    let rec mkKn (ty: Type) =
+        if FSharpType.IsFunction(ty) then
+            let _, ran = FSharpType.GetFunctionElements(ty)
+            // NOTICE: do not delay `mkKn` invocation until runtime
+            let f = mkKn ran
+            FSharpValue.MakeFunction(ty, fun _ -> f)
+        else
+            box ()
+    
+    [<Sealed>]
+    type Format<'T> private () =
+        static let instance : 'T =
+            unbox (mkKn typeof<'T>)
+        static member Instance = instance
+    
+    let inline dprint verbose args =
+        if verbose then
+            printfn args
+        else
+            Format<_>.Instance
