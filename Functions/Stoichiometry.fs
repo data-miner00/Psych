a18cb26d7facd2339e55a96c04c282de2eeb1b94
data-miner00 <data-miner00@users.noreply.github.com>
2022-02-16 20:26:43 +0800

    feat: Added stoichiometry rate equation

---
+++ Functions/Stoichiometry.fs
@@ -0,0 +1,26 @@
+﻿namespace Psych.Functions
+
+module Stoichiometry =
+
+    open System
+    open Common
+
+    // k is decay constant
+    let halfLife k =
+        ln 2. / k
+
+    module RateEquation =
+        
+        // ln N = -kt + ln N0
+
+        let findT N k N0 =
+            (ln N - ln N0) / -k
+
+        let findN k t N0 =
+             Math.Pow(Math.E, (-k * t + ln N0))
+             
+        let findK N t N0 =
+            (ln N - ln N0) / -t
+
+        let findN0 N t k =
+            ln N + k * t
