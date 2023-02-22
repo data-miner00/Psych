74dfafcd00fc0bfc625a228bade5330fb77be033
data-miner00 <data-miner00@users.noreply.github.com>
2023-02-22 21:34:43 +0800

    docs: Added some showcase

---
+++ Playground/Exercism.fs
@@ -0,0 +1,24 @@
+﻿module Exercism
+
+open System
+
+// [WARN]: Message
+
+let message (logLine: string): string =
+    logLine
+    |> fun x -> x.Split(": ")
+    |> fun x -> x.GetValue(1)
+    |> fun x -> x.ToString()
+    |> fun x -> x.Trim()
+
+let logLevel(logLine: string): string =
+    logLine
+    |> fun x -> x.Split(": ")
+    |> fun x -> x.GetValue(0)
+    |> fun x -> x.ToString()
+    |> Seq.filter Char.IsLetter
+    |> String.Concat
+    |> fun x -> x.ToLower()
+
+let reformat(logLine: string): string =
+    sprintf "%s (%s)" (message logLine) (logLevel logLine)
--- Playground/Program.fs
+++ Playground/Program.fs
@@ -4,6 +4,18 @@ open System
 open Psych.Functions
 
 module Program =
+
+    (* Built-in math functions *)
+    let transientContext =
+        let res = abs -1
+        let res = ceil 4.5
+        let res = floor 4.5
+        let res = log 2.718281828
+        let res = log10 1000.0
+        let res = sqrt 25.0
+
+        0
+
     [<EntryPoint>]
     let main argv =
         let str = "Hello World from F#!"
