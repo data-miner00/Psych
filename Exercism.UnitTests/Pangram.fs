a4e0d34d6832934a687e682501aec6f8dab9500e
data-miner00 <data-miner00@users.noreply.github.com>
2023-02-24 01:34:02 +0800

    test: Added exercism unit test

---
+++ Exercism.UnitTests/Pangram.fs
@@ -0,0 +1,29 @@
+namespace Psych.Exercism.UnitTests
+
+module Tests =
+
+    open Xunit
+    open Psych.Exercism
+
+    [<Theory>]
+    [<InlineData "The quick brown fox jumps over the lazy dog.">]
+    [<InlineData "ABCDEFGHIJKLMNOPQRSTUVWXYZ">]
+    [<InlineData "abcdefghijlkmnopqrstuvwxyz">]
+    [<InlineData "abcdefghijlkmnopqrstuvwxyza">]
+    [<InlineData "abCdEfGHijKlMnOPQrSTuvwXYz">]
+    [<InlineData "ABCDE FGHIJ  KLMNO PQRSTUVWX.&&YZ__">]
+    let ``Pangram should return true when input is a pangram`` input =
+        input
+        |> Pangram.isPangram
+        |> Assert.True
+
+    [<Theory>]
+    [<InlineData "">]
+    [<InlineData " ">]
+    [<InlineData "!#@$%#&$&%(%)$&$&#(#">]
+    [<InlineData "ABCDEFGHIJKLMNOPQRSTUVWXY">]
+    [<InlineData "BCDEFGHIJKLMNOPQRSTUVWXYZ">]
+    let ``Pangram should return false when input is not a pangram`` input =
+        input
+        |> Pangram.isPangram
+        |> Assert.False
