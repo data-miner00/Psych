79b4f37bf5120706e3d527f58e4264f6b2efc7ab
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-03 09:33:31 +0800

    feat: Added Ackermann formula

---
+++ Functions.UnitTests/Recursion.fs
@@ -0,0 +1,16 @@
+﻿namespace Psych.Functions.UnitTests
+
+open Psych.Functions
+open Xunit
+
+module RecursionTest =
+    [<Theory>]
+    [<InlineData(0, 0, 1)>]
+    [<InlineData(1, 2, 4)>]
+    [<InlineData(2, 1, 5)>]
+    [<InlineData(2, 2, 7)>]
+    [<InlineData(3, 4, 125)>]
+    [<InlineData(4, 1, 65535)>]
+    let ``Ackermann Test`` m n (e: int) =
+        Recursion.ackermann m n 
+        |> fun x -> Assert.Equal(e, x)
---
+++ Functions/Recursion.fs
@@ -0,0 +1,17 @@
+﻿namespace Psych.Functions
+
+open System
+
+module Recursion =
+    let rec ackermann m n =
+        match (m, n) with
+        | (a, b) when a = 0 -> b + 1
+        | (a, b) when a > 0 && b = 0 -> ackermann (a - 1) 1
+        | (a, b) when a > 0 && b > 0 -> ackermann (a - 1) (ackermann a b-1)
+        | _ -> raise (ArgumentException("Something Wrong"))
+
+    let rec ackermann' m n =
+        match m, n with
+        | 0, n -> n + 1
+        | m, 0 -> ackermann' (m - 1) 1
+        | m, n -> ackermann' (m - 1) (ackermann' m (n - 1))
