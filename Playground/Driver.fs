0fbf41fba1fc00b7a3b196b8f3d3e70f7d762b62
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-04 12:24:14 +0800

    feat: Improved driver version

--- Playground/Driver.fs
+++ Playground/Driver.fs
@@ -27,3 +27,38 @@ module UserConsole =
                 loop balance
         loop 0m
         ()
+
+    // Improved version with error handling
+    let private getAmount'() =
+        Console.Write("Enter the amount of the transaction: ")
+        Console.ReadLine() |> Decimal.TryParse
+        |> fun x ->
+            match x with
+            | true, value -> Ok value
+            | false, _ -> Error "Cant parse"
+
+    let run'() =
+        let rec loop balance =
+            printfn "Balance: %A" balance
+
+            let action = "w"
+            printfn "You told me to do this: %A" action
+
+            match action with
+            | "d" | "w" ->
+                match getAmount'() with
+                | Ok value ->
+                    match action with
+                    | "d" -> loop (balance + value)
+                    | "w" -> loop (balance - value)
+                    | _ -> loop balance
+                | Error message ->
+                    printfn "%A" message
+                    loop balance
+            | "x" -> ()
+            | _ ->
+                printfn $"Invalid action: {action}"
+                loop balance
+
+        loop 0m
+        ()
