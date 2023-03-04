842846bb9c8df977682820c730368f329ad37d71
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-04 09:21:54 +0800

    docs: Add access attributes

--- Concepts/Structure/Module.fs
+++ Concepts/Structure/Module.fs
@@ -1,5 +1,6 @@
 ﻿namespace Psych.Concepts.Structure
 
+[<AutoOpen>]
 module Transaction =
     // Type definition
     type T =
@@ -8,3 +9,11 @@ module Transaction =
     // Methods associated with type
     let create id amount =
         { Id = id; Amount = amount }
+
+[<RequireQualifiedAccess>] // Use when feel like name will conflict; Use for readibility
+module Transaction' =
+    type T =
+        { Id: int; Name: string }
+
+    let create id name =
+        { Id = id; Name = name }
