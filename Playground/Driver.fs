39bd83e2879e7744a4698064186df72736de7c6b
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-04 09:22:40 +0800

    docs: Added console UI driver example

---
+++ Playground/Driver.fs
@@ -0,0 +1,29 @@
+﻿namespace Psych.Playground
+
+/// <summary>
+/// A sample implementation for Console Interface application
+/// with recursion and immutability.
+/// </summary>
+module UserConsole =
+    open System
+
+    let private getAmount () =
+        Console.Write("Enter the amount of the transaction: ")
+        Console.ReadLine() |> Decimal.Parse
+
+    let run() =
+        let rec loop balance =
+            printfn "Balance: %A" balance
+
+            let action = "w"
+            printfn "You told me to do this: %A" action
+
+            match action with
+            | "d" -> loop (balance + getAmount())
+            | "w" -> loop (balance - getAmount())
+            | "x" -> ()
+            | _ ->
+                printfn $"Invalid action: {action}"
+                loop balance
+        loop 0m
+        ()
