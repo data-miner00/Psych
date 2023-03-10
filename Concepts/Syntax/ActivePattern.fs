57df9fb783415ead2551c77e75be3748d5a939b9
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-10 19:22:53 +0800

    docs: Added simple active pattern

---
+++ Concepts/Syntax/ActivePattern.fs
@@ -0,0 +1,28 @@
+﻿namespace Psych.Concepts.Syntax
+
+open System.Text.RegularExpressions
+
+module ActivePattern =
+    let (|Even|Odd|) input = if input % 2 = 0 then Even else Odd
+
+    let testNumber input =
+        match input with
+        | Even -> printfn "%d is even" input
+        | Odd -> printfn "%d is odd" input
+
+
+    (* Regex *)
+    let (|Match|_|) pattern input =
+        let m = Regex.Match(input, pattern) in
+        if m.Success then Some (List.tryLast [for g in m.Groups -> g.Value]) else None
+
+    let containsUrl value =
+        match value with
+        | Match "(http:\/\/S+)" result -> Some result
+        | _ -> None
+
+    let isPhone value =
+        match value with
+        | Match @"\(([0-9]{3})\)[-. ]?([0-9]{3})[-. ]?([0-9]{4})" a ->
+            Some (a)
+        | _ -> None
