c50a9f4ce5adffb5eef376ac68b70d9af3675870
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-10 18:25:51 +0800

    feat: Added grains

---
+++ Exercism/Grains.fs
@@ -0,0 +1,22 @@
+﻿namespace Psych.Exercism
+
+open System.Numerics
+
+module Grains =
+    let square (n: int): Result<uint64,string> =
+        if n > 64 || n <= 0 then
+            Error "square must be between 1 and 64"
+        else
+            BigInteger.Pow(2UL, n - 1) |> uint64 |> Ok
+
+    let unwrap (res: Result<uint64, string>) =
+        match res with
+        | Ok num -> num
+        | Error(_) -> 0UL
+
+    let total: Result<uint64,string> =
+        [1..64] 
+        |> List.map square
+        |> List.map unwrap
+        |> List.fold (+) 0UL
+        |> Ok
