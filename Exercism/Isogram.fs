746191cee1e7d77692f0634b8fee8f74c678b9d2
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-10 09:38:56 +0800

    feat: Added isogram

---
+++ Exercism/Isogram.fs
@@ -0,0 +1,12 @@
+﻿namespace Psych.Exercism
+
+open System
+
+module Isogram =
+
+    let isIsogram (str: string) =
+        let filtered = str.ToUpper() |> String.filter (Char.IsLetter)
+
+        filtered.ToCharArray()
+        |> Set.ofArray
+        |> fun x -> x.Count = filtered.Length
