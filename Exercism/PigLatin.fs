1e591bd977e5df0998c8edb25790caa1333e1f4a
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-27 22:12:15 +0800

    feat: Unoptimized Pig latin

---
+++ Exercism/PigLatin.fs
@@ -0,0 +1,34 @@
+﻿namespace Psych.Exercism
+
+open System
+
+module PigLatin =
+    let translate (inputs: string) =
+        let vowels = ["a"; "e"; "i"; "o"; "u"; "xr"; "yt"]
+        inputs.Split(' ')
+        |> Array.map (fun input ->
+            match List.tryFind (fun x -> input.StartsWith(x, StringComparison.OrdinalIgnoreCase)) vowels with
+            | Some _ ->
+                input + "ay"
+            | None ->
+                let augmentedVowel = 
+                    if input.StartsWith('y') then
+                        [|'a'; 'e'; 'i'; 'o'; 'u'|]
+                    else [|'a'; 'e'; 'i'; 'o'; 'u'; 'y'|]
+                let firstVowelIndex = input.IndexOfAny(augmentedVowel)
+                let quIndex = input.IndexOf("qu")
+            
+                let consonantCluster =
+                    if quIndex = -1 then
+                        input.Substring(0, firstVowelIndex)
+                    else
+                        input.Substring(0, quIndex + 2)
+
+                let rest =
+                    if quIndex = -1 then
+                        input.Substring(firstVowelIndex)
+                    else
+                        input.Substring(quIndex + 2)
+
+                rest + consonantCluster + "ay")
+        |> String.concat " "
