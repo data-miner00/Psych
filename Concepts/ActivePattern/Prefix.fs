065b3904e0d3c8e7fa0608e465ca2570e0b59304
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-27 19:29:50 +0800

    docs: Added active pattern prefix example

---
+++ Concepts/ActivePattern/Prefix.fs
@@ -0,0 +1,13 @@
+﻿namespace Psych.Concepts.ActivePattern
+
+module Prefix =
+    let (|Prefix|_|) (p:string) (s:string) =
+        if s.StartsWith(p) then
+            Some(s.Substring(p.Length))
+        else
+            None
+
+    match "Hello world" with
+    | Prefix "The" rest -> printfn "Started with 'The', rest is %s" rest
+    | Prefix "Hello" rest -> printfn "Started with 'Hello', rest is %s" rest
+    | _ -> printfn "neither"
