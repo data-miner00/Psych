2760a4495bb0bf12ce6fb104303dfc8a7be1ff98
data-miner00 <data-miner00@users.noreply.github.com>
2023-06-14 09:56:10 +0800

    feat: Added Collatz Conjecture exercise

---
+++ Exercism.UnitTests/Easy/CollatzConjectureTests.fs
@@ -0,0 +1,31 @@
+﻿namespace Psych.Exercism.UnitTests.Easy
+
+open Xunit
+open FsUnit.Xunit
+open Psych.Exercism.Easy.CollatzConjecture
+
+module CollatzConjectureTests =
+    [<Fact>]
+    let ``Zero steps for one`` () =
+        steps 1 |> should equal (Some 0)
+
+    [<Fact>]
+    let ``Divide if even`` () =
+        steps 16 |> should equal (Some 4)
+
+    [<Fact>]
+    let ``Even and odd steps`` () =
+        steps 12 |> should equal (Some 9)
+
+    [<Fact>]
+    let ``Large number of even and odd steps`` () =
+        steps 1000000 |> should equal (Some 152)
+
+    [<Fact>]
+    let ``Zero is an error`` () =
+        steps 0 |> should equal None
+
+    [<Fact>]
+    let ``Negative value is an error`` () =
+        steps -15 |> should equal None
+
---
+++ Exercism/Easy/CollatzConjecture.fs
@@ -0,0 +1,15 @@
+﻿namespace Psych.Exercism.Easy
+
+module CollatzConjecture =
+    let rec private helper (current: int) (acc: int): int =
+        if current = 1 then
+            acc
+        else
+            helper (if current % 2 = 0 then current / 2 else current * 3 + 1) (acc + 1)
+
+    let steps (number: int): int option =
+        if number < 1 then
+            None
+        else
+            helper number 0 |> Some
+
