7888c2e77d7db1acea90d491db17521567772d75
data-miner00 <data-miner00@users.noreply.github.com>
2023-06-14 09:00:41 +0800

    feat: Added armstrong number exercise

---
+++ Exercism.UnitTests/Easy/ArmstrongNumberTests.fs
@@ -0,0 +1,43 @@
+﻿namespace Psych.Exercism.UnitTests.Easy
+
+open Xunit
+open FsUnit.Xunit
+open Psych.Exercism.Easy.ArmstrongNumber
+
+module ArmstrongNumberTests =
+    [<Fact>]
+    let ``Zero is an Armstrong number`` () =
+        isArmstrongNumber 0 |> should equal true
+
+    [<Fact>]
+    let ``Single-digit numbers are Armstrong numbers`` () =
+        isArmstrongNumber 5 |> should equal true
+
+    [<Fact>]
+    let ``There are no two-digit Armstrong numbers`` () =
+        isArmstrongNumber 10 |> should equal false
+
+    [<Fact>]
+    let ``Three-digit number that is an Armstrong number`` () =
+        isArmstrongNumber 153 |> should equal true
+
+    [<Fact>]
+    let ``Three-digit number that is not an Armstrong number`` () =
+        isArmstrongNumber 100 |> should equal false
+
+    [<Fact>]
+    let ``Four-digit number that is an Armstrong number`` () =
+        isArmstrongNumber 9474 |> should equal true
+
+    [<Fact>]
+    let ``Four-digit number that is not an Armstrong number`` () =
+        isArmstrongNumber 9475 |> should equal false
+
+    [<Fact>]
+    let ``Seven-digit number that is an Armstrong number`` () =
+        isArmstrongNumber 9926315 |> should equal true
+
+    [<Fact>]
+    let ``Seven-digit number that is not an Armstrong number`` () =
+        isArmstrongNumber 9926314 |> should equal false
+
---
+++ Exercism/Easy/ArmstrongNumber.fs
@@ -0,0 +1,12 @@
+﻿namespace Psych.Exercism.Easy
+
+module ArmstrongNumber =
+    
+    let isArmstrongNumber (number: int): bool =
+        let numberAsString = number.ToString()
+
+        numberAsString
+        |> Seq.map(fun x -> (pown (int x - int '0') numberAsString.Length))
+        |> Seq.reduce (+)
+        |> (=) number
+
