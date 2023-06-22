4cf8c16e8b3fe972f30430cee42a7dc98168b469
data-miner00 <data-miner00@users.noreply.github.com>
2023-06-22 18:04:24 +0800

    feat: Added perfect number

---
+++ Exercism/Easy/PerfectNumber.fs
@@ -0,0 +1,19 @@
+﻿namespace Psych.Exercism.Easy
+
+module PerfectNumber =
+    type Classification = Perfect | Abundant | Deficient
+
+    let classify n =
+        if n < 1 then
+            None
+        else
+            let factors = 
+                [ for i in 1 .. n - 1 do
+                    if n % i = 0 then yield i]
+            let sum = factors |> List.sum
+            
+            match sum with
+            | _ when sum > n -> Some Abundant
+            | _ when sum < n -> Some Deficient
+            | _ -> Some Perfect
+
