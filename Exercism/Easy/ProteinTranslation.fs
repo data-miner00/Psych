dce04e594bbea51435a4c3b8b23738aabc1089ca
data-miner00 <data-miner00@users.noreply.github.com>
2023-06-22 08:51:22 +0800

    feat: Added protein translation

---
+++ Exercism/Easy/ProteinTranslation.fs
@@ -0,0 +1,22 @@
+﻿namespace Psych.Exercism.Easy
+
+module ProteinTranslation =
+    let private findProtein (codon: string) =
+        match codon with
+        | "AUG" -> "Methionine"
+        | "UUU" | "UUC" -> "Phenylalanine"
+        | "UUA" | "UUG" -> "Leucine"
+        | "UCU" | "UCC" | "UCA" | "UCG" -> "Serine"
+        | "UAU" | "UAC" -> "Tyrosine"
+        | "UGU" | "UGC" -> "Cysteine"
+        | "UGG" -> "Tryptophan"
+        | "UAA" | "UAG" | "UGA" | _ -> "STOP"
+
+    let proteins (rna: string) =
+        rna.ToCharArray()
+        |> Seq.chunkBySize 3
+        |> Seq.map System.String
+        |> Seq.map findProtein
+        |> Seq.takeWhile ((<>) "STOP")
+        |> List.ofSeq
+
